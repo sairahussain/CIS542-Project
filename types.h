83cb0b308ea67c8f990642379cf4642e45d0df9b
Rahul Nafde <rahulanafde@gmail.com>
2014-04-11 19:01:54 -0400

    types.h
    Header file for node plan containing node costs and direction, map for storing routes to be given to the boebot.

---
+++ types.h
@@ -0,0 +1,24 @@
+
+#include <map>
+#include <deque>
+#include <vector>
+
+
+
+typedef struct Plan{
+    size_t cost;   // INT_MAX denotes infinity
+    char direction; // i denotes infinity, 'N','W','S','E' are respective directions
+}Plan;
+
+typedef vector< map <size_t,Plan> > nodeMap;
+nodeMap nodeGraph;
+ 
+typedef struct PathNode{
+    size_t NodeID;
+    size_t cost;   // INT_MAX denotes infinity
+    char direction; // i denotes infinity, 'N','W','S','E' are respective directions
+}PathNode;
+
+void setNodes();
+
+typedef map<size_t,deque<size_t> > routeMap; 
